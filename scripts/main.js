c72f450fadde22f8a75cc2bd6e2ae7dc1b046457
Nicolas A. Oyarzabal <79150521+nicky-eng@users.noreply.github.com>
2022-01-02 17:13:05 -0300

    Add skill badges (#1)
    * Added skill-badge images in media folder
    
    * Added images to index in a single column list
    
    * Added skill badges and footer.

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,12 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function () {
     let mySrc = myImage.getAttribute('src');
-    if (mySrc === 'images/nicky-headshot.png') {
-        myImage.setAttribute('src', 'images/nicky-helmet.png');
+    if (mySrc === 'media/nicky-headshot.png') {
+        myImage.setAttribute('src', 'media/nicky-helmet.png');
+        myImage.setAttribute('alt', 'A side portrait of Nicolas wearing a white helmet in an industrial plant.');
     } else {
-        myImage.setAttribute('src', 'images/nicky-headshot.png');
+        myImage.setAttribute('src', 'media/nicky-headshot.png');
+        myImage.setAttribute('alt', 'A portrait of Nicolas in a black t-shirt');
     }
 }
 
